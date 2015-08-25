22b608e224464e1ad4e514355cc7106abdb4071d
zhanhb <zhanhb8@qq.com>
2015-08-25 21:01:54 +0800

    update html.

--- judge-web/src/main/java/com/github/zhanhb/judge/util/Strings.java
+++ judge-web/src/main/java/com/github/zhanhb/judge/util/Strings.java
@@ -43,20 +43,6 @@ public class Strings {
                 : Math.max(0, str.length() + index);
     }
 
-    @SuppressWarnings("NestedAssignment")
-    public static boolean isBlank(final CharSequence cs) {
-        int strLen;
-        if (cs == null || (strLen = cs.length()) == 0) {
-            return true;
-        }
-        for (int i = 0; i < strLen; i++) {
-            if (!Character.isWhitespace(cs.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     private Strings() {
         throw new AssertionError();
     }
