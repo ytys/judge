2b875bf93abe089529c9e459cb5a5b2314101ca8
zhanhb <zhanhb8@qq.com>
2015-08-27 21:04:20 +0800

    exclude commons-logging, remove jdbc ds log.

---
+++ judge-web/src/test/java/com/github/zhanhb/judge/security/SecurityConfigTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 zhanhb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.zhanhb.judge.security;
+
+import com.github.zhanhb.judge.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ *
+ * @author zhanhb
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class SecurityConfigTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    @Autowired
+    private FilterChainProxy filterChain;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = webAppContextSetup(wac)
+                .addFilter(filterChain)
+                .build();
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
