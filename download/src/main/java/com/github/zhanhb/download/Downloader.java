c4dd03cac92f9a3ba77e6c98b29a9543497745e3
zhanhb <zhanhb88@gmail.com>
2016-01-26 13:04:56 +0800

    [download] remove commons-io

--- download/src/main/java/com/github/zhanhb/download/Downloader.java
+++ download/src/main/java/com/github/zhanhb/download/Downloader.java
@@ -27,7 +27,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpHeaders;
 
 @Slf4j
